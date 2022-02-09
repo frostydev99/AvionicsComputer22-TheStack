972bab9f446b192af8271572d9906a7b99822b3c
napin <napin@130.215.168.177>
2022-02-08 20:04:42 -0500

    Created NEOM9N peripheral class

---
+++ src/peripherals/NEOM9N.cpp
@@ -0,0 +1,12 @@
+/*
+ * NEOM9N.cpp
+ *
+ * Created on: Feb 8, 2022
+ * Author: Natanel Pinkhasov
+ */
+
+#include "NEOM9N.h"
+
+
+
+
---
+++ src/peripherals/NEOM9N.h
@@ -0,0 +1,18 @@
+/*
+ * NEO-M9N.h
+ *
+ * Created on: Feb 8, 2022
+ * Author: Natanel Pinkhasov
+ */
+
+#ifndef SRC_PERIPHERALS_NEOM9N_H_
+#define SRC_PERIPHERALS_NEOM9N_H_
+
+#include "Arduino.h"
+#include "Wire.h"
+
+class NEOM9N {
+
+};
+
+#endif /* SRC_PERIPHERALS_NEOM9N_H_ */
